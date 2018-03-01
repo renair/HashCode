6008dd3d19cbefad534f64e7c4f5f0bf81fd15ea
renair <platinium9889@gmail.com>
2018-03-01 21:46:12 +0200

    Main alpha implemented

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import objects as obj
 import obj_parser as pars
 import simulation
+import tools
 
 model, rides = pars.parse_file("a_example.in")
-print(test1)
+
 
 #Відсортувати список поїздок
 sm = Simulation()
 #Цикл:
+for v in sm.vehicles:
+	ride = get_ride_for_vehicle(v, 0, rides, model.steps, model.per_time_bonus)
+	sm.addRide(ride, v)
+
+while sm.time < model.steps or len(rides) == 0:
 #Додати поїздки до симуляції
 #Запустити симуляцію
-	sm.simulate()
+	free_cars = sm.simulate()
+	for v in free_cars:
+		ride = get_ride_for_vehicle(v, sm.time, rides, model.steps, model.per_time_bonus)
+		sm.addRide(ride, v)
 #Взяти позиції автомобілів із симуляції
