6e1dddd1d34a385c503b63eec8b87e7ed41015c8
renair <platinium9889@gmail.com>
2018-03-01 20:48:58 +0200

    Fix simulation.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import objects as obj
+import obj_parser as pars
+
+test1 = pars.parse_file("a_example.in")
+print(test1)
--- tools.py
+++ tools.py
@@ -1,4 +1,2 @@
 def time_to_arrive(fr, to):
     return abs(to[0] - fr[0]) + abs(to[1] - fr[1])
-
-print time_to_arrive((0, 0), (2, 2))
