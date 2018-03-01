70cee32db256f55505e99945959373ba30de15c8
i800 <usatschoff@gmail.com>
2018-03-01 21:15:12 +0200

    get_max_points complete

--- tools.py
+++ tools.py
@@ -4,22 +4,21 @@ def time_to_arrive(fr, to):
 
 def get_max_points(start_pos, client_pos, finish_pos,\
                    curr_time, earl_start, latest_fin,\
-                   num_steps):
+                   num_steps, bonus):
     # How much time we have for the ride.
-    max_time = latest_fin - earl_start
+    max_steps_curr = latest_fin - earl_start
     # How much time we have till the end
     # of the simulation.
     max_steps_left = num_steps - curr_time
+    # How many steps we need to get the client.
+    time_to_client = time_to_arrive(start_pos, client_pos)
+    # How many steps we need to get the destination.
+    time_to_finish = time_to_arrive(client_pos, finish_pos)
     # How many scores we can get in case we arrive in time.
-    default_scores = time_to_arrive(start_pos, finish_pos)
+    default_scores = time_to_client + time_to_finish
     
-    if (max_time <= 0 or max_steps_left < default_score):
+    if (max_steps_curr <= 0 or max_steps_left < default_score):
         return 0
     
-    time_to_client = time_to_arrive(start_pos, client_pos)
-    time_to_finish = time_to_arrive(client_pos, finish_pos)
-    
-    
-    
-    return 1
+    return default_scores + bonus
 
